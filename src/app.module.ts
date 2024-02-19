ae56a848b1f96afd0530ace33135edf1a8044611
shunich <fukusyun3@gmail.com>
2024-02-19 20:09:19 +0900

    add: models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      database: 'todoappdb',
+      username: 'testuser',
+      password: 'password',
+      entities: [__dirname + '/**/*.model{.ts,.js}'],
+      synchronize: true, // 本番環境ではデータが消える可能性があるので、使用しない方が良い
+      logging: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
